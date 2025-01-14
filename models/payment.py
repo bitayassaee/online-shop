e0bf4f931f72e1c9d16014577e5ed73c208fddd9
bita <bitays200@gmail.com>
2025-01-14 19:22:52 +0330

    cart,cart_items and payments MOUDELS added

---
+++ models/payment.py
@@ -0,0 +1,15 @@
+from sqlalchemy import *
+from extentions import db
+
+
+class Payment(db.Model):
+    __tablename__ = "payments"
+    id = Column(Integer, primary_key=True)
+    status = Column(string, default="Pending",)
+    cart_id = Column(Integer, ForeignKey("carts.id"), nullable=False)
+    cart = db.relationship("Cart", backref="payments")
+
+
+
+
+
