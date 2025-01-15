22971bd2b94a30f6521a0a10911437ea8dbad8fd
bita <bitays200@gmail.com>
2025-01-15 18:55:04 +0330

    user cart added

--- blueprints/user.py
+++ blueprints/user.py
@@ -1,28 +1,33 @@
-from flask import Blueprint,render_template,request,redirect,url_for,flash
-from flask_login import login_user
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import login_user, login_required, current_user
 from passlib.hash import sha256_crypt
 from extentions import db
+from models.cart import Cart
+from models.cart_item import CartItem
+from models.product import Product
 from models.user import User
-app = Blueprint("user",__name__)
 
-@app.route('/user/login', methods=['GET','POST'])
+app = Blueprint("user", __name__)
+
+
+@app.route('/user/login', methods=['GET', 'POST'])
 def login():
     if request.method == "GET":
         return render_template('user/login.html')
     else:
-        register = request.form.get('register',None)
-        username = request.form.get('username',None)
-        password = request.form.get('password',None)
-        phone = request.form.get('phone',None)
-        address = request.form.get('address',None)
+        register = request.form.get('register', None)
+        username = request.form.get('username', None)
+        password = request.form.get('password', None)
+        phone = request.form.get('phone', None)
+        address = request.form.get('address', None)
 
-        if register != None :
+        if register != None:
             user = User.query.filter(User.username == username).first()
             if user != None:
                 flash('نام کاربری دیگری انتخاب کنید')
                 return redirect(url_for('user.login'))
 
-            user = User(username =username, password = sha256_crypt.encrypt(password),phone =phone,address =address)
+            user = User(username=username, password=sha256_crypt.encrypt(password), phone=phone, address=address)
             db.session.add(user)
             db.session.commit()
             login_user(user)
@@ -42,6 +47,41 @@ def login():
                 return redirect(url_for('user.login'))
         return 'done'
 
+
+
+
+
+@app.route('/add-to-cart', methods=['GET'])
+@login_required
+def add_to_cart():
+    id = request.args.get('id')
+    product = Product.query.filter(Product.id == id).first_or_404()
+
+    cart = current_user.carts.filter(Cart.status == 'pending').first()
+    if cart == None:
+        cart = Cart()
+        current_user.carts.append(cart)
+        db.session.add(cart)
+
+    cart_item = cart.cart_items.filter(CartItem.product == product).first()
+    if cart_item == None:
+        item = CartItem(quantity=1)
+        item.cart = cart
+        item.product = product
+        db.session.add(item)
+    else:
+        cart_item.quantity += 1
+
+    db.session.commit()
+
+    return redirect(url_for('user.cart'))
+
+@app.route('/cart', methods=['GET'])
+@login_required
+def cart():
+    return render_template('user/cart.html')
+
 @app.route('/user/dashboard', methods=['GET'])
+@login_required
 def dashboard():
     return "thi is dashboard"
--- models/cart.py
+++ models/cart.py
@@ -1,13 +1,14 @@
 from sqlalchemy import *
+from sqlalchemy.orm import backref
 from extentions import db
 
 
 class Cart(db.Model):
     __tablename__ = "carts"
     id = Column(Integer, primary_key=True)
-    status = Column(String, default="Pending",)
+    status = Column(String, default='pending',)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    user = db.relationship("User", backref="carts")
+    user = db.relationship("User", backref=backref("carts", lazy="dynamic"))
 
 
 
